7c18f1df0400a1aacc27a88cf794418dcc2258a2
karson-mann <112511533+karson-mann@users.noreply.github.com>
2022-12-01 14:26:55 -0600

    part of part 2

--- Day01/Day01.java
+++ Day01/Day01.java
@@ -3,43 +3,66 @@ import java.io.File;
 
 public class Day01 {
 
-    private final static String DATA_FILE = "input.txt";
+    private final static String DATA_FILE = "sampleInput.txt";
 
     public static void part1() {
         Scanner in;
-        try{
-            in=new Scanner(new File(DATA_FILE));
-        } catch(Exception e){
+        try {
+            in = new Scanner(new File(DATA_FILE));
+        } catch (Exception e) {
             System.out.print(e);
             return;
         }
 
-        int most=0;
+        int most = 0;
+    
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            int calories = 0;
 
-        while(in.hasNextLine()){
-            String line =in.nextLine();
-            int calories=0;
+            // get calories not seperated by a space
+            while (!line.equals("") && in.hasNext()) {
+                calories += Integer.parseInt(line);
+                line = in.nextLine();
 
-            //get calories not seperated by a space
-            while(!line.equals("")&&in.hasNext()){
-                calories+=Integer.parseInt(line);
-                line=in.nextLine();
-                
-                
             }
-            if(calories>most){
-                most=calories;
+            if (calories > most) {
+                most = calories;
             }
         }
         System.out.println(most);
     }
 
-
     public static void part2() {
-        
-    }
+        Scanner in;
+        try {
+            in = new Scanner(new File(DATA_FILE));
+        } catch (Exception e) {
+            System.out.print(e);
+            return;
+        }
 
-    public static void main(String[] args){
+        ArrayList
+    
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            int calories = 0;
+
+            // get calories not seperated by a space
+            while (!line.equals("") && in.hasNext()) {
+                calories += Integer.parseInt(line);
+                line = in.nextLine();
+
+            }
+            if (calories > most) {
+                most = calories;
+            }
+        }
+        System.out.println(most);
+    }
+    
+
+    public static void main(String[] args) {
         part1();
     }
 
