40d94eb8638642db54f263c27dd3018fb5a47f43
karson-mann <112511533+karson-mann@users.noreply.github.com>
2022-12-01 14:18:29 -0600

    one gold star

--- Day01/Day01.java
+++ Day01/Day01.java
@@ -1,7 +1,37 @@
+import java.util.Scanner;
+import java.io.File;
+
 public class Day01 {
 
-    public static void part1() {
+    private final static String DATA_FILE = "input.txt";
 
+    public static void part1() {
+        Scanner in;
+        try{
+            in=new Scanner(new File(DATA_FILE));
+        } catch(Exception e){
+            System.out.print(e);
+            return;
+        }
+
+        int most=0;
+
+        while(in.hasNextLine()){
+            String line =in.nextLine();
+            int calories=0;
+
+            //get calories not seperated by a space
+            while(!line.equals("")&&in.hasNext()){
+                calories+=Integer.parseInt(line);
+                line=in.nextLine();
+                
+                
+            }
+            if(calories>most){
+                most=calories;
+            }
+        }
+        System.out.println(most);
     }
 
 
@@ -9,6 +39,8 @@ public class Day01 {
         
     }
 
-
+    public static void main(String[] args){
+        part1();
+    }
 
 }
