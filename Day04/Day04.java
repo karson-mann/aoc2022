a95a8acf694f5ce1458f0aa179e0c123bfb32e7f
karson-mann <karson.mann10298@gmail.com>
2022-12-06 00:41:01 -0600

    one gold star

--- Day04/Day04.java
+++ Day04/Day04.java
@@ -1,68 +1,86 @@
-class Day04 {
+import java.io.*;
 
-    public static void main(String[] args) {
-        //new Day4().part1();
-        new Day04().part2();
-    }
+public class Day04
+{
 
-    public void part1() {
-        try (RandomAccessFile file = new RandomAccessFile("/home/ulrich/projects/aoc/2022/java/src/day4/input", "r")) {
-            String str;
+    static int linecount = 0;
+    static int totalcount = 0;
+    static int overlapcount = 0;
+    static BufferedReader reader;
+    public static void main(String[] args)
+    {   String filename = "input.txt";
+        if(args.length>0)
+        filename=args[args.length-1];
+        String line = "";
+    
+        try
+        {
+            BufferedReader br=new BufferedReader(new FileReader(filename));
+            line = br.readLine();
+            while(line != null){
+                linecount++;
+        
+        if(processString(line)>0)
+                totalcount++;
+                if(processString1(line)>0)
+                overlapcount++;
+                    line = br.readLine();
 
-            int sum = 0;
-
-            Pattern regex = Pattern.compile("([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)");
-
-            while ((str = file.readLine()) != null) {
-                Matcher matcher = regex.matcher(str);
-
-                if (matcher.find()) {
-                    int elf1_left = parseInt(matcher.group(1)),
-                            elf1_right = parseInt(matcher.group(2));
-                    int elf2_left = parseInt(matcher.group(3)),
-                            elf2_right = parseInt(matcher.group(4));
-
-                    if ((elf1_left <= elf2_left && elf2_right <= elf1_right)
-                            || (elf2_left <= elf1_left && elf1_right <= elf2_right)) {
-                        sum++;
-                    }
-                }
-            }
-            System.out.println(sum);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            
         }
-    }
-
-    public void part2() {
-        try (RandomAccessFile file = new RandomAccessFile("/home/ulrich/projects/aoc/2022/java/src/day4/input", "r")) {
-            String str;
-
-            int sum = 0;
-
-            Pattern regex = Pattern.compile("([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)");
-
-            while ((str = file.readLine()) != null) {
-                Matcher matcher = regex.matcher(str);
-
-                if (matcher.find()) {
-                    int elf1_left = parseInt(matcher.group(1)),
-                            elf1_right = parseInt(matcher.group(2));
-                    int elf2_left = parseInt(matcher.group(3)),
-                            elf2_right = parseInt(matcher.group(4));
-
-                    // 5-7,7-9
-                    // 2-6,4-8
-                    // 6-6,4-6
-                    // 2-6,4-8
-                    if ((elf1_left <= elf2_right && elf1_right >= elf2_left)) {
-                        sum++;
-                    }
-                }
-            }
-            System.out.println(sum);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            br.close();
         }
+        catch(Exception e){}
+        System.out.println(totalcount);
+        System.out.println(overlapcount);
+        }
+
+    public static int processString(String s)
+    {
+    
+    try{
+    int v1 = Integer.parseInt(s.split(",")[0].split("-")[0]);
+    int v2 = Integer.parseInt(s.split(",")[0].split("-")[1]);
+    int v3 = Integer.parseInt(s.split(",")[1].split("-")[0]);
+    int v4 = Integer.parseInt(s.split(",")[1].split("-")[1]);
+    if(isConsumed(v1, v2, v3, v4)||isConsumed(v3, v4, v1, v2))
+            return 1;
+    else return 0;
     }
+    catch(Exception e){}
+        
+        return -1;
+    }
+
+    public static int processString1(String s)
+    {
+    
+    try{
+    int v1 = Integer.parseInt(s.split(",")[0].split("-")[0]);
+    int v2 = Integer.parseInt(s.split(",")[0].split("-")[1]);
+    int v3 = Integer.parseInt(s.split(",")[1].split("-")[0]);
+    int v4 = Integer.parseInt(s.split(",")[1].split("-")[1]);
+    if(isbetween(v1, v2, v3, v4)||isbetween(v3, v4, v1, v2))
+            return 1;
+        else return 0;
+    }
+    catch(Exception e){}
+        
+        return -1;
+    }
+
+    public static boolean isbetween(int v1, int v2, int v3, int v4)
+    {
+    if((v1>=v3 && v1 <=v4) || (v2>=v3 && v2 <=v4))
+    return true;
+    return false;
+    }
+
+    public static boolean isConsumed(int v1, int v2, int v3, int v4)
+    {
+        if((v2<=v4)&&(v1>=v3))
+            return true;
+        return false;
+        }
+
 }
